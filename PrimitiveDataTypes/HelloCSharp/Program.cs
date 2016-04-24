b9bc689689a38e644bf2b67dcb52b8b9e60b0208
rodi1i <rstoqnow@gmail.com>
2016-04-24 17:04:33 +0300

    implemented greeting feature

--- PrimitiveDataTypes/HelloCSharp/Program.cs
+++ PrimitiveDataTypes/HelloCSharp/Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloCSharp
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Hello C#");
     }
 }
